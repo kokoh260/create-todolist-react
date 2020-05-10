d0c0cd0acc74e4c3df290261adcb2a72b317946d
kokoh260 <59475062+kokoh260@users.noreply.github.com>
2020-05-10 21:25:02 +0700

    membuat starter

--- src/App.js
+++ src/App.js
@@ -3,9 +3,54 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div class="container">
+    <div class="frame">
+      <section class="header">
+        <button class="header-btn">TAMBAH</button>
+        <h1 class="header-title">Catatan Kerja</h1>
+        <button class="header-btn main-red-color">HAPUS</button>
+      </section>
+
+      <section class="add">
+        <form class="add-form">
+          <input type="text" class="add-input" />
+          <button class="add-btn main-black-color">TAMBAH</button>
+        </form>
+      </section>
+
+      <section class="todos">
+        <div class="todo">
+          <span class="todo-text">Belajar react</span>
+        </div>
+        <div class="todo">
+          <span class="todo-text">Belajar react</span>
+        </div>
+        <div class="todo">
+          <span class="todo-text">Belajar react</span>
+        </div>
+        <div class="todo">
+          <span class="todo-text">Belajar react</span>
+        </div>
+        <div class="todo">
+          <span class="todo-text">Belajar react</span>
+        </div>
+        <div class="todo">
+          <span class="todo-text">Belajar react</span>
+        </div>
+        <div class="todo">
+          <span class="todo-text">Belajar react</span>
+        </div>
+        <div class="todo">
+          <span class="todo-text">Belajar react</span>
+        </div>
+        <div class="todo">
+          <span class="todo-text">Belajar react</span>
+        </div>
+        <div class="todo">
+          <span class="todo-text">Belajar react</span>
+        </div>
+      </section>
     </div>
+  </div>
   );
 }
